dcd0fe2d9f083fbc4cb59e326e84f7ffe62b3b00
StenixDev <stenix.dev@gmail.com>
2026-02-10 06:33:48 +0800

    [sdev] - installing emailjs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,54 +1,39 @@
 import { Mail } from 'lucide-react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Rocket } from 'lucide-react';
+import { projects } from '../data/projects';
+import emailjs from '@emailjs/browser';
 
-const projects = [
-  {
-    type: 'Adaptable',
-    title: 'Adaptable',
-    description:
-      'I’m not locked into one stack. I learn what your project needs.',
-    tech: ['React', 'Next.js', 'Tailwind', 'TypeScript'],
-    status: 'In Progress',
-  },
-  {
-    type: 'Clear Communication',
-    title: 'Clear Communication',
-    description:
-      'I keep things transparent, simple, and honest throughout the process.',
-    tech: ['AWS', 'Docker', 'Kubernetes', 'CI/CD'],
-    status: 'Ongoing',
-  },
-  {
-    type: 'Long-Term Mindset',
-    title: 'Long-Term Mindset',
-    description:
-      'I care about building relationships and growing with your project over time.',
-    tech: ['OpenAI', 'Python', 'TensorFlow', 'FastAPI'],
-    status: 'Research',
-  },
-];
 function Contact() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
+  const [isSending, setIsSending] = useState<boolean>(false);
   const year = new Date().getFullYear();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
-    alert('Message sent! (This is a demo)');
-    setFormData({ name: '', email: '', message: '' });
+
+    if (!form.current) return;
+
+    setIsSending(true);
+
+    emailjs
+      .sendForm('service_wcb7mxh', 'template_uxhombi', form.current!, {
+        publicKey: 'ZhjpIv5vuzI16M4jH',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      )
+      .finally(() => {
+        setIsSending(false);
+      });
   };
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
   return (
     <section id='contact' className='px-6 py-10 md:pt-32 pb-10  bg-zinc-900/50'>
       <div className='max-w-6xl mx-auto'>
@@ -86,14 +71,12 @@ function Contact() {
               developer who’s flexible and eager to grow, I’d love to hear about
               it.
             </p>
-            <form onSubmit={handleSubmit} className='space-y-6'>
+            <form onSubmit={sendEmail} ref={form} className='space-y-6'>
               <div>
                 <label className='block text-sm text-zinc-400 mb-2'>Name</label>
                 <input
                   type='text'
                   name='name'
-                  value={formData.name}
-                  onChange={handleChange}
                   className='w-full bg-zinc-900 border border-zinc-800 rounded px-4 py-3 focus:outline-none focus:border-lime-400 transition-colors'
                   placeholder='Your name'
                 />
@@ -105,8 +88,6 @@ function Contact() {
                 <input
                   type='email'
                   name='email'
-                  value={formData.email}
-                  onChange={handleChange}
                   className='w-full bg-zinc-900 border border-zinc-800 rounded px-4 py-3 focus:outline-none focus:border-lime-400 transition-colors'
                   placeholder='your.email@example.com'
                 />
@@ -118,14 +99,13 @@ function Contact() {
                 <textarea
                   rows={6}
                   name='message'
-                  value={formData.message}
-                  onChange={handleChange}
                   className='w-full bg-zinc-900 border border-zinc-800 rounded px-4 py-3 focus:outline-none focus:border-lime-400 transition-colors resize-none'
                   placeholder='Tell me about your project...'
                 ></textarea>
               </div>
               <div className='flex justify-end'>
                 <button
+                  disabled={isSending}
                   type='submit'
                   className='w-full md:w-auto px-8 py-3 bg-lime-400 text-zinc-950 rounded hover:bg-lime-500 transition-colors font-medium'
                 >
